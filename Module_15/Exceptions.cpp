101225c886b49908cc2aa601264217d93b5f59de
Jon-Luke Ogayonne <jon-luke.ogayonne@sinclair.edu>
2021-05-03 23:21:02 -0400

    I have added a file header for the cctype to receive the char data. the try/catch construct is updated to throw not just an invalid range exception, but also an invalid character exception. It is modified to throw an exception if the char return type has a different case than its start type.

--- Module_15/Exceptions.cpp
+++ Module_15/Exceptions.cpp
@@ -4,31 +4,53 @@
 	May 01, 2021
 */
 #include<iostream>
+#include<cctype>
 using namespace std;
 // Prototype for program.
 char character(char, int);
 // Program starts at main.
 int main() {
-	//
-	char letter = 'a';
+	// Character and Integer for the main.
+	char letter = 'a', charValue;
 	int number = 1;
-	//
+	// Try/catch construct to determine the outcome letter or exception.
 	try
 	{
-		character(letter, number);
-		cout << " 'b'" << endl;
+		charValue = character (letter, number);
+		cout << " '" << charValue << "'" << endl;
 	}
-	catch (const std::exception&)
+	catch (string exception)
 	{
-
+		cout << exception << endl;
 	}
 	// Terminate program.
 	return 0;
 }
+// Function to calculate letter value and range.
 char character(char start, int offset) {
+	// If statement to throw if character is not valid.
 	if (start < 'A' || start > 'Z' && start < 'a' || start > 'z') {
-		string invalidRangeExpection = " ERROR! Invalid range.\n";
-		throw invalidRangeExpection;
+		string invalidCharacterException = " ERROR! Invalid Charactor.\n";
+		throw invalidCharacterException;
 	}
-		return start;
+	// Add the offset to the character ascii value.
+	int result = offset + static_cast<int>(start);
+	// Convert integer result into character value.
+	static_cast<char>(result);
+	// Test if character value is uppercase.
+	if (isupper(start)) {
+		if (!isupper(result)) {
+			string invalidRangeException = " ERROR! Invalid range.\n";
+			throw invalidRangeException;
+		}
+	}
+	// Test if character value is lowercase.
+	else if (islower(start)) {
+		if (!islower(result)) {
+			string invalidRangeException = " ERROR! Invalid range.\n";
+			throw invalidRangeException;
+		}
+	}
+	// Return result.
+	return result;
 }
